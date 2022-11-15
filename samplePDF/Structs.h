5bebcedb002147aa8f03089366183750874389d0
EdAtkin <edhuthwaite@aol.com>
2022-11-15 11:43:45 +0000

    removing some structs and enums that were used for T2K

--- samplePDF/Structs.h
+++ samplePDF/Structs.h
@@ -105,213 +105,6 @@ struct FastSplineInfo {
   int flat;
 };
 
-// *******************
-// The kinematic types to be enjoyed by all
-enum KinematicTypes {
-// ******************
-  kLeptonMomentum        = 0,
-  kLeptonCosTheta        = 1,
-  kNeutrinoEnergy        = 2,
-  kQ2                    = 3,
-  kErecQE                = 4,
-  kQ2QE                  = 5,
-  kLeptonTheta           = 6,
-  kq0                    = 7,
-  kq3                    = 8,
-  kProtonMomentumReco    = 9,
-  kProtonCosThetaReco    = 10,
-  kPionMomentum          = 11,
-  kPionCosTheta          = 12,
-  kPionMomentumFS        = 13,
-  kProtonMomentumFS      = 14,
-  kLeptonTrueTheta       = 15,
-  kW                     = 16,
-  kW2                    = 17,
-  kM3Mode                = 18,
-  kOscChannel            = 19,
-  kErec                  = 20,
-  //kPDFBinning has to be the final one
-  kPDFBinning            = 21,
-  kNKinematicParams   
-};
-
-
-// *******************
-// Get the name of a kinematic type
-// Useful for setting x and y axis of TH2D samples and so on
-inline std::string Kinematic_ToString(KinematicTypes type) {
-  // *******************
-  std::string ReturnString = "";
-  switch(type) {
-    case kLeptonMomentum:
-      ReturnString = "p_{#mu} (MeV)";
-      break;
-    case kLeptonCosTheta:
-      ReturnString = "cos #theta_{#mu}";
-      break;
-    case kNeutrinoEnergy:
-      ReturnString = "E_{#nu}^{true} (GeV)";
-      break;
-    case kQ2:
-      ReturnString = "Q^{2}_{true} (GeV^{2})";
-      break;
-    case kErecQE:
-      ReturnString = "E_{#nu}^{QE} (GeV)";
-      break;
-    case kQ2QE:
-      ReturnString = "Q^{2}_{QE} (GeV^{2})";
-      break;
-    case kLeptonTheta:
-      ReturnString = "#theta_{lep} (degrees)";
-      break;
-    case kq0:
-      ReturnString = "|q_{0}| (Mev)";
-      break;
-    case kq3:
-      ReturnString = "|q_{3}| (Gev)";
-      break;
-    case kProtonMomentumReco:
-      ReturnString = "p_{proton} (MeV)";
-      break;
-    case kProtonCosThetaReco:
-      ReturnString = "cos #theta_{proton}";
-      break;
-    case kPionMomentum:
-      ReturnString = "High. interaction p_{#pi^{#pm}}(MeV)";
-      break;
-    case kPionCosTheta:
-      ReturnString = "cos #theta_{#pi}";
-      break;
-    case kPionMomentumFS:
-      ReturnString = "High. FS p_{#pi^{#pm}}(MeV)";
-      break;
-    case kProtonMomentumFS:
-      ReturnString = "True proton momentum in Final State (MeV)";
-      break;
-    case kLeptonTrueTheta:
-      ReturnString = "True #theta_{lep} (degrees)";
-      break;
-    case kW2:
-      ReturnString = "W^{2} (GeV^{2})";
-      break;
-    case kW:
-      ReturnString = "W (GeV)";
-      break;
-    case kM3Mode:
-      ReturnString = "Mode";
-      break;
-    case kOscChannel:
-      ReturnString = "Oscillation Channel";
-      break;
-    case kPDFBinning:
-      std::cout << "You shouldn't be here because PDF binning is different for each sample." << std::endl;
-      std::cout << "Given kPDFBinning. Exitting.." << std::endl;
-      throw;
-    default:
-      std::cerr << "Error, did not find ToString conversion for " << type << std::endl;
-      break;
-  }
-  return ReturnString;
-};
-
-inline double StringToKinematicVar(std::string kinematic_str){
-
-  if(strcmp(kinematic_str.c_str(), "Etrue") == 0){
-	return kNeutrinoEnergy;
-  }
-  else if(strcmp(kinematic_str.c_str(), "Q2true") == 0){
-	return kQ2;
-  }	
-  else{
-	std::cerr << "[ERROR] " << __FILE__ << ":" << __LINE__ << " Unknown string " << kinematic_str << " passed from xsec cov" << std::endl; 
-	std::cerr << "Have you added this kinematic parameter into StringToKinematicVar?" << std::endl;
-	throw;
-  }
-
-  return 0;
-}
-
-
-
-// Useful for printing out ot pngs or writing out to histograms
-inline std::string Kinematic_ToShortString(KinematicTypes type) {
-  // *******************
-  std::string ReturnString = "";
-  switch(type) {
-    case kLeptonMomentum:
-      ReturnString = "LepMom";
-      break;
-    case kLeptonCosTheta:
-      ReturnString = "CosThetaLep";
-      break;
-    case kNeutrinoEnergy:
-      ReturnString = "TrueEnu";
-      break;
-    case kQ2:
-      ReturnString = "TrueQ2";
-      break;
-    case kErecQE:
-      ReturnString = "TrueEnuQE";
-      break;
-    case kQ2QE:
-      ReturnString = "TrueQ2QE";
-      break;
-    case kLeptonTheta:
-      ReturnString = "ThetaLep";
-      break;
-    case kq0:
-      ReturnString = "q0";
-      break;
-    case kq3:
-      ReturnString = "q3";
-      break;
-    case kProtonMomentumReco:
-      ReturnString = "ProtonMom";
-      break; 
-    case kProtonCosThetaReco:
-      ReturnString = "CosThetaProton";
-      break; 
-    case kPionMomentum:
-      ReturnString = "PiMomInt";
-      break;
-    case kPionCosTheta:
-      ReturnString = "CosThetaPi";
-      break;
-    case kPionMomentumFS:
-      ReturnString = "LeadingPiMomFS";
-      break;
-    case kProtonMomentumFS:
-      ReturnString = "ProtonMomFS";
-      break;
-    case kLeptonTrueTheta:
-      ReturnString = "TrueThetaLep";
-      break;
-    case kW2:
-      ReturnString = "TrueW2";
-      break;
-    case kW:
-      ReturnString = "TrueW";
-      break;
-    case kM3Mode:
-      ReturnString = "Mode";
-      break;
-    case kOscChannel:
-      ReturnString = "OscChannel";
-      break;
-    case kErec:
-      ReturnString = "Erec";
-      break;
-    case kPDFBinning:
-      std::cout << "You shouldn't be here because PDF binning is different for each sample." << std::endl;
-      std::cout << "Given kPDFBinning. Exitting.." << std::endl;
-      throw;
-    default:
-      std::cerr << "Error, did not find ToShortString conversion for " << type << std::endl;
-      break;
-  }
-  return ReturnString;
-};
-
 // ********************************************
 // Generic xsec class
 // Can use TF1 or TSpline3 or TSpline5 here, tjoho
@@ -379,7 +172,7 @@ class XSecStruct{
     T* Func;
 };
 
->>>>>>> f2086eb0ae778357e504213492d518845909cdaf
+
 // ************************
 // A reduced TF1 class only
 // Only saves parameters for each TF1 and how many parameters each parameter set has
@@ -1189,7 +982,7 @@ enum TargetMat {
   kTarget_N  = 14,
   kTarget_O  = 16,
   kTarget_Al = 27,
-  kTarget_Ar = 40
+  kTarget_Ar = 40,
   kTarget_Ti = 48,
   kTarget_Fe = 56,
   kTarget_Pb = 207
@@ -1335,7 +1128,6 @@ class MaCh3_Modes {
     virtual int Mode_ToSplineMode(int i);
 };
 
->>>>>>> f2086eb0ae778357e504213492d518845909cdaf
 // Helper function for calculating unbinned Integral of TH2Poly i.e including overflow
 double OverflowIntegral(TH2Poly*);
 
