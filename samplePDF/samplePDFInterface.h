48db1744001b8a011e256a173f22d45ccd6f6b5c
EdAtkin <edhuthwaite@aol.com>
2023-07-04 14:19:03 +0100

    removing reweight function for nu and anti-nu are treated seperately for now. The oscillator class will deal with this I think

--- samplePDF/samplePDFInterface.h
+++ samplePDF/samplePDFInterface.h
@@ -18,7 +18,6 @@ class samplePDFInterface
 {
  public:
   virtual void reweight(double *oscpar)=0;
-  virtual void reweight(double *oscpar, double *oscpar2)=0;
 
   virtual double getEventRate()=0;
  
@@ -41,9 +40,6 @@ class samplePDFInterface
   virtual double getLikelihood_kernel(std::vector<double> &data)=0;
   virtual void fill1DHist()=0;
   virtual void fill2DHist()=0;
-
-//  virtual TH1D *nominalSpectrum1D=0;
-//  virtual TH2D *nominalSpectrum2D=0;
 };
 
 #endif
