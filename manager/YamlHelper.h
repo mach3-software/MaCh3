7a6a85b85b10eed46318868321f40230be292e2e
Kamil Skwarczynski <skwarczynskikamil@gmail.com>
2024-03-15 01:16:31 +0100

    Even further expand yaml helper, at this point it is manager to the manager...

--- manager/YamlHelper.h
+++ manager/YamlHelper.h
@@ -16,33 +16,54 @@ Type GetFromManager(const YAML::Node& node, Type defval)
   return tmpNode.as<Type>();
 }
 
+// Use this like this CheckNodeExists(config, "LikelihoodOptions", "TestStatistic");
 //KS: Base case for recursion
 template<typename T>
-bool checkPathExistsHelper(const T& node) {
+bool CheckNodeExistsHelper(const T& node) {
   (void)node;
   return true; // Node exists, return true
 }
 
 //KS: Recursive function to traverse YAML nodes
 template<typename T, typename... Args>
-bool checkPathExistsHelper(const T& node, const std::string& key, Args... args) {
+bool CheckNodeExistsHelper(const T& node, const std::string& key, Args... args) {
   if (!node[key]) {
     //std::cerr << "Node " << key << " doesn't exist." << std::endl;
     return false;
   }
-  return checkPathExistsHelper(node[key], args...);
+  return CheckNodeExistsHelper(node[key], args...);
 }
 
 //KS: Wrapper function to call the recursive helper
 template<typename... Args>
-bool checkPathExists(const YAML::Node& node, Args... args) {
-  return checkPathExistsHelper(node, args...);
+bool CheckNodeExists(const YAML::Node& node, Args... args) {
+  return CheckNodeExistsHelper(node, args...);
 }
 
-//KS: WARNING Don't use this. It works but tmpNode.as<Type>() will delete YAML node... need to figure out how to fix this
-template<typename Type, typename... Args>
-Type FindFromManager(const YAML::Node& node, Args... args) {
-  YAML::Node tmpNode = node;
-  ((tmpNode = tmpNode[args]), ...);
-  return tmpNode.as<Type>();
+
+
+/// Use this like this FindFromManager<std::string>(config, "LikelihoodOptions", "TestStatistic");
+
+// Base case for recursion
+template<typename T>
+T FindFromManagerHelper(const YAML::Node& node) {
+  return node.as<T>(); // Convert YAML node to the specified type
 }
+
+// Recursive function to traverse YAML nodes
+template<typename T, typename... Args>
+T FindFromManagerHelper(const YAML::Node& node, const std::string& key, Args... args) {
+  if (!node[key]) {
+    std::cerr << "Node " << key << " doesn't exist." << std::endl;
+    throw;
+    return T();
+  }
+  return FindFromManagerHelper<T>(node[key], args...); // Recursive call
+}
+
+// Wrapper function to call the recursive helper
+template<typename T, typename... Args>
+T FindFromManager(const YAML::Node& node, Args... args) {
+  return FindFromManagerHelper<T>(node, args...);
+}
+
