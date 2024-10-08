d1e8ee75c5b5d782a023413765b8da24195da058
Ewan <ewan.miller@kcl.ac.uk>
2024-10-08 10:44:33 -0700

    first stab at getting sphinx documentation to generate for the python module

---
+++ Doc/sphinx/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'pyMaCh3'
+copyright = '2024, Ewan Miller'
+author = 'Ewan Miller'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon"
+    ]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ Doc/sphinx/source/plotting.rst
@@ -0,0 +1,7 @@
+plotting module
+===============
+
+.. automodule:: pyMaCh3.plotting
+   :members:
+   :undoc-members:
+   :show-inheritance:
