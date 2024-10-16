88669d2fe47836301b851568a1793132709a4cc6
Ewan <ewan.miller@kcl.ac.uk>
2024-10-16 06:46:56 -0700

    move some description stuff to actual documentation rather than just a docstring

--- python/samplePDF.cpp
+++ python/samplePDF.cpp
@@ -140,10 +140,7 @@ void initSamplePDF(py::module &m){
 
     auto m_sample_pdf = m.def_submodule("sample_pdf");
     m_sample_pdf.doc() = 
-        "This module deals with sampling from the posterior density function of your particular experimental model at different points, given your data. \
-        In order to do this, you will generally need to create a SamplePDF object derived from :py:class:`pyMaCh3.fitter.SamplePDFFDBase` for each sample of events for your experiment. \
-        For some more details on this you can see [the wiki page](https://github.com/mach3-software/MaCh3/wiki/04.-Making-a-samplePDF-experiment-class) on this. The code examples there are written using c++ however the general ideas are the same. \
-        Happy sampling!";
+        "This is a Python binding of MaCh3s C++ based samplePDF library.";
 
     // Bind the systematic type enum that lets us set different types of systematics
     py::enum_<TestStatistic>(m_sample_pdf, "TestStatistic")
