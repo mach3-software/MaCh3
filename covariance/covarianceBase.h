1840c76f6d9e0f285dceae833d729b1ae3444294
Luke Pickering <luketpickering@gmail.com>
2024-10-31 11:46:23 +0000

    undo re-add of deprecated code. oops

--- covariance/covarianceBase.h
+++ covariance/covarianceBase.h
@@ -285,8 +285,7 @@ class covarianceBase {
     //KS: Transfer to normal base
     TransferToParam();
   }
-  /// @brief Get number of params in normal Base, if you want something which will work with PCA as well please use getNpars()
-  inline int getSize() { return _fNumPar; }
+
   /// @brief Get number of params which will be different depending if using Eigen decomposition or not
   inline int getNpars() {
     if (pca) return _fNumParPCA;
@@ -387,8 +386,6 @@ protected:
   /// The input root file we read in
   const std::string inputFile;
 
-  /// Total number of params, deprecated, please don't use it
-  int size;
   /// Name of cov matrix
   std::string matrixName;
   /// The covariance matrix
