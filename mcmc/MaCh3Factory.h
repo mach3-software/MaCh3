c91961887b5912842f5ddae0b54e6af13140f471
Kamil Skwarczynski <skwarczynskikamil@gmail.com>
2024-11-03 16:52:12 +0000

    implement sample PDF factory

--- mcmc/MaCh3Factory.h
+++ mcmc/MaCh3Factory.h
@@ -25,7 +25,7 @@
 std::unique_ptr<FitterBase> MaCh3FitterFactory(manager *fitMan, std::vector<samplePDFBase>& Samples, std::vector<covarianceBase>& Covariances);
 
 
-/// @brief Factory function for creating a covariance matrix for systematic handling.
+/// @brief Factory function for creating a covariance class for systematic handling.
 ///
 /// @param fitMan Pointer to the manager class that holds the configuration settings.
 /// @param name Prefix, for example Xsec, then code will look for XsecCovFile
@@ -52,4 +52,34 @@ std::unique_ptr<FitterBase> MaCh3FitterFactory(manager *fitMan, std::vector<samp
 /// @todo add adaptive stuff
 covarianceXsec* MaCh3CovarianceFactory(manager *fitMan, const std::string& PreFix);
 
+/// @brief Factory function for creating SamplePDF and initialisation with systematic.
+///
+/// @tparam SampleType The class type of the sample to create, e.g., `samplePDFTutorial`.
+/// @param SampleConfig Path to sample config.
+/// @param xsec A pointer to a covarianceXsec object for cross-section systematic settings.
+/// @param osc (Optional) A pointer to a covarianceOsc object for oscillation systematic settings.
+/// @return A pointer to the created SampleType object, initialized and ready for use.
+///
+/// @example
+/// ```cpp
+/// samplePDFTutorial* mySample = MaCh3SamplePDFFactory<samplePDFTutorial>(SampleConfig, xsec, osc);
+/// ```
+template <typename SampleType>
+SampleType* MaCh3SamplePDFFactory(const std::string& SampleConfig, covarianceXsec* xsec, covarianceOsc* osc = nullptr)
+{
+  // Instantiate the sample using the specified class type
+  SampleType* Sample = new SampleType(SampleConfig[0], xsec);
+  Sample->SetXsecCov(xsec);
+  if (osc != nullptr) Sample->SetOscCov(osc);
+  Sample->reweight();
 
+  // Obtain sample name and create a TString version for histogram naming
+  std::string name = Sample->GetName();
+  TString NameTString = TString(name.c_str());
+
+  // Clone the 1D histogram with a modified name
+  TH1D* SampleHistogramPrior = static_cast<TH1D*>(Sample->get1DHist()->Clone(NameTString + "_Prior"));
+  Sample->addData(SampleHistogramPrior);
+
+  return Sample;
+}
