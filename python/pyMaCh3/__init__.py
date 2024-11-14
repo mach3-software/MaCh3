bb5f61e0a30a6f371a7a3f7271d088ee2135435a
Ewan Miller <ewan.miller@kcl.ac.uk>
2024-11-14 08:36:26 +0100

    oops forgot to include covariance module in __init__.py

--- python/pyMaCh3/__init__.py
+++ python/pyMaCh3/__init__.py
@@ -1,3 +1,3 @@
-from ._pyMaCh3 import __doc__, fitter, manager, plotting, sample_pdf, splines
+from ._pyMaCh3 import __doc__, fitter, manager, plotting, sample_pdf, splines, covariance
 
-__all__ = ["__doc__", "fitter", "manager", "plotting", "sample_pdf", "splines"]
+__all__ = ["__doc__", "fitter", "manager", "plotting", "sample_pdf", "splines", "covariance"]
