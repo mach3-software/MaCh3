963cc1cd6154733a987e70200313eabc3bf99731
EdAtkin <edhuthwaite@aol.com>
2024-12-05 11:59:21 +0000

    updating factory method to not include SetXsecCov, since it's templated this wasn't being flagged as an error in the core but popped up in the CI

--- mcmc/MaCh3Factory.h
+++ mcmc/MaCh3Factory.h
@@ -124,8 +124,7 @@ std::vector<SampleType*> MaCh3SamplePDFFactory(const std::vector<std::string>& S
   for (size_t i = 0; i < SampleConfig.size(); ++i)
   {
     // Instantiate the sample using the specified class type
-    SampleType* Sample = new SampleType(SampleConfig[i], xsec);
-    Sample->SetXsecCov(xsec);
+    SampleType* Sample = new SampleType(SampleConfig[i], xsec, osc);
     if (osc != nullptr) Sample->SetOscCov(osc);
     Sample->reweight();
 
