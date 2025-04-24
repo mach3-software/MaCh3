e6ce7e12d4eddd31a32a3cb6ccc8b4c527b5294e
Kamil Skwarczynski <skwarczynskikamil@gmail.com>
2025-04-24 16:12:06 +0000

    Add limits for ubuntu

--- manager/Core.h
+++ manager/Core.h
@@ -12,6 +12,8 @@
 #include <vector>
 #include <iomanip>
 #include <cmath>
+#include <limits>
+
 /// Run low or high memory versions of structs
 /// N.B. for 64 bit systems sizeof(float) == sizeof(double) so not a huge effect
 namespace M3 {
