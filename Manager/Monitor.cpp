3a31bd9c3ea6274d1dc40edba07e8268486e60e3
Kamil Skwarczynski <skwarczynskikamil@gmail.com>
2025-08-25 10:07:06 +0000

    ups...

--- Manager/Monitor.cpp
+++ Manager/Monitor.cpp
@@ -367,7 +367,7 @@ int GetNThreads() {
 void AddPath(std::string& FilePath) {
 // ***************************************************************************
   //KS:Most inputs are in ${MACH3}/inputs/blarb.root
-  if (std::getenv("MACH3") != nullptr) {
+  if (std::getenv("MACH3") == nullptr) {
     MACH3LOG_ERROR("MACH3 is not defined");
     MACH3LOG_ERROR("Please source your setup script");
     MACH3LOG_ERROR("Read more: https://github.com/mach3-software/MaCh3/wiki/0.-FAQ#error-need-mach3-environment-variable");
