5d0134584d7564d1341a0a509c27d4a11d7826e4
KSkwarczynski <KSkwarczynski@users.noreply.github.com>
2025-09-15 13:16:18 +0000

    deploy: 5408ca06b8a5b1a19911f64b614b2e35d89286fa

---
+++ .Archive/v2.0.0/search/files_4.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['fardetectorcoreinfostruct_2eh_1689',['FarDetectorCoreInfoStruct.h',['../FarDetectorCoreInfoStruct_8h.html',1,'']]],
+  ['fitterbase_2ecpp_1690',['FitterBase.cpp',['../FitterBase_8cpp.html',1,'']]],
+  ['fitterbase_2eh_1691',['FitterBase.h',['../FitterBase_8h.html',1,'']]],
+  ['fitters_2ecpp_1692',['fitters.cpp',['../fitters_8cpp.html',1,'']]]
+];
---
+++ .Archive/v2.1.0/search/classes_8.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['oscchannelinfo_1694',['OscChannelInfo',['../structOscChannelInfo.html',1,'']]],
+  ['oscillationhandler_1695',['OscillationHandler',['../classOscillationHandler.html',1,'']]],
+  ['oscprocessor_1696',['OscProcessor',['../classOscProcessor.html',1,'']]]
+];
