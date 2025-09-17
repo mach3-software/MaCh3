5e0ff6a398bbb8fe73ffd7f8aae0f013644609f0
KSkwarczynski <KSkwarczynski@users.noreply.github.com>
2025-09-17 17:47:15 +0000

    deploy: 62884913e6c1396c12f843f90391a270aa968c87

---
+++ .Archive/v1.1.0/search/typedefs_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['float_5ft_3398',['FLOAT_T',['../OscClass__CUDAProb3_8h.html#ae859fcc828db5cc21dc6bd9611634949',1,'OscClass_CUDAProb3.h']]]
+];
---
+++ .Archive/v2.1.0/search/classes_9.js
@@ -0,0 +1,17 @@
+var searchData=
+[
+  ['parameterhandlerbase_1697',['ParameterHandlerBase',['../classParameterHandlerBase.html',1,'']]],
+  ['parameterhandlergeneric_1698',['ParameterHandlerGeneric',['../classParameterHandlerGeneric.html',1,'']]],
+  ['parameterhandlerosc_1699',['ParameterHandlerOsc',['../classParameterHandlerOsc.html',1,'']]],
+  ['parametertunes_1700',['ParameterTunes',['../classParameterTunes.html',1,'']]],
+  ['particle_1701',['particle',['../classparticle.html',1,'']]],
+  ['pcahandler_1702',['PCAHandler',['../classPCAHandler.html',1,'']]],
+  ['plottingmanager_1703',['PlottingManager',['../classMaCh3Plotting_1_1PlottingManager.html',1,'MaCh3Plotting']]],
+  ['pso_1704',['PSO',['../classPSO.html',1,'']]],
+  ['pyfitterbase_1705',['PyFitterBase',['../classPyFitterBase.html',1,'']]],
+  ['pylikelihoodfit_1706',['PyLikelihoodFit',['../classPyLikelihoodFit.html',1,'']]],
+  ['pyparameterhandlerbase_1707',['PyParameterHandlerBase',['../classPyParameterHandlerBase.html',1,'']]],
+  ['pysamplehandlerbase_1708',['PySampleHandlerBase',['../classPySampleHandlerBase.html',1,'']]],
+  ['pysamplehandlerfd_1709',['PySampleHandlerFD',['../classPySampleHandlerFD.html',1,'']]],
+  ['pysplinebase_1710',['PySplineBase',['../classPySplineBase.html',1,'']]]
+];
