dc32fad6b948ea6987baef285c8bcea5fbfb88e6
Alexander Richards <inferno_alex@hotmail.com>
2025-09-22 15:12:30 +0100

    Update SplineMonolith.cpp

--- splines/SplineMonolith.cpp
+++ splines/SplineMonolith.cpp
@@ -15,7 +15,7 @@ class IDOptimized;
 
 // Minimum capacity of a pipe.
 // Set to 0 to allow the compiler to save area if possible.
-constexpr size_t kPipeMinCapacity = 4096;
+constexpr size_t kPipeMinCapacity = 32;
 
 // Pipes
 struct PipeStruct{
