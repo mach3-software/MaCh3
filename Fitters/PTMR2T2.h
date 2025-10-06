2ff095cefbbbf26e888a6bdd13c10661d62df9b4
Henry Wallace <henryi@narval2.narval.calcul.quebec>
2025-10-06 08:40:18 -0400

    Add notes

--- Fitters/PTMR2T2.h
+++ Fitters/PTMR2T2.h
@@ -3,8 +3,28 @@
 #include "Fitters/MR2T2.h"
 
 /*
+
+
 https://arxiv.org/pdf/1205.1076
 */
+
+/// @brief Parallel Tempered (Adaptive) MR2T2 algorith
+/// @details
+/// Run several chains in parallel at different temperatures
+/// Temperature is a scales the posterior to
+/// \f[
+/// \pi(x)_{T} = \pi(x)^{1/T}
+/// \f]
+/// Chains of different temperatures can swap values with probability
+/// \f[
+/// \alpha = \min\left(1, \frac{\pi(x_i)^{1/T_j} \pi(x_j)^{1/T_i}}{\pi(x_i)^{1/T_i} \pi(x_j)^{1/T_j}}\right)
+/// \f]
+/// The algorithm aims to get a swap rates of ~25% which is roughly optimal
+/// The only "true" chain is the \f$T=1\f$ chain which samples from the true posterior
+///
+/// @note This implementation is based on MR2T2 but could easily be adapted to other MCMC algorithms
+
+/// @author Henry Wallace
 
 #ifdef MPIENABLED
 class PTMR2T2 : public MR2T2
