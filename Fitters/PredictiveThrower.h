44082b34baff37993fc17f7456313c1d4b3bcbec
Henry Wallace <henryi@login1.int.fir.alliancecan.ca>
2025-12-12 03:46:35 -0800

    Remove hacks to PredictiveThrower from an early iteration

--- Fitters/PredictiveThrower.h
+++ Fitters/PredictiveThrower.h
@@ -102,9 +102,6 @@ class PredictiveThrower : public FitterBase {
 
   /// Number of toys we are generating analysing
   int Ntoys;
-
-  bool SampleWholeChain;
-
   /// KS: Names of parameter groups that will not be varied
   std::vector<std::string> ParameterGroupsNotVaried;
   /// KS: Index of parameters that will be varied
