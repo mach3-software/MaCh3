68a7d12d90366b8e5028131811aac2db6f087114
Charlotte Knight <mdk16@ic.ac.uk>
2025-12-18 13:52:45 +0000

    fix ignored-attributes warning

--- Manager/Monitor.cpp
+++ Manager/Monitor.cpp
@@ -179,7 +179,14 @@ std::string TerminalToString(std::string cmd) {
 // ************************
   std::array<char, 128> buffer;
   std::string result;
-  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
+  
+  struct PCloseDeleter {
+    void operator()(FILE* f) const {
+      if (f) pclose(f);
+    }
+  };
+  std::unique_ptr<FILE, PCloseDeleter> pipe(popen(cmd.c_str(), "r"));
+
   if (!pipe) {
     throw MaCh3Exception(__FILE__, __LINE__, "popen() failed!");
   }
