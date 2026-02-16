daef61a24e1a4a5908cc7e92641955aa143ca78f
Kamil Skwarczynski <skwarczynskikamil@gmail.com>
2026-02-16 09:03:18 +0000

    After chat with Dan impvoe start from previous chain

--- Fitters/FitterBase.cpp
+++ Fitters/FitterBase.cpp
@@ -350,7 +350,7 @@ void FitterBase::StartFromPreviousFit(const std::string& FitName) {
   MACH3LOG_INFO("Getting starting position from {}", FitName);
   TFile *infile = M3::Open(FitName, "READ", __FILE__, __LINE__);
   TTree *posts = infile->Get<TTree>("posteriors");
-  int step_val = 0;
+  unsigned int step_val = 0;
   double log_val = M3::_LARGE_LOGL_;
   posts->SetBranchAddress("step",&step_val);
   posts->SetBranchAddress("LogL",&log_val);
@@ -397,7 +397,7 @@ void FitterBase::StartFromPreviousFit(const std::string& FitName) {
     }
   }
   logLCurr = log_val;
-
+  logLProp = log_val;
   delete posts;
   infile->Close();
   delete infile;
