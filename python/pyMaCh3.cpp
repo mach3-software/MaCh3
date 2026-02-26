4fe83d3c00747b703d5cab4a2c561b875ca09482
Ewan Miller <ewan.miller@kcl.ac.uk>
2026-02-26 16:43:17 +0000

    tidy up python binding system. Now should be possible for experiment to implement their own MACh3PyBinder class that can be used to do their own bindings

--- python/pyMaCh3.cpp
+++ python/pyMaCh3.cpp
@@ -1,23 +1,6 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
+#include "pyMaCh3.h"
 
 /// @file pyMaCh3.cpp
 /// @author Ewan Miller
 
-namespace py = pybind11;
-
-void initPlotting(py::module &); // <- defined in python/plotting.cpp
-void initFitters(py::module &); // <- defined in python/fitters.cpp
-void initSamples(py::module &); // <- defined in python/samples.cpp
-void initManager(py::module &); // <- defined in python/manager.cpp
-void initParameters(py::module &); // <- defined in python/parameters.cpp
-void initSplines(py::module &);  // <- defined in python/splines.cpp
-
-PYBIND11_MODULE( _pyMaCh3, m ) {
-    initPlotting(m);
-    initFitters(m);
-    initSamples(m);
-    initManager(m);
-    initParameters(m);
-    initSplines(m);
-}
+MAKE_PYMACH3_MDULE( MaCh3PyBinder )
