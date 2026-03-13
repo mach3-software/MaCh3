a269c6fab96b6326074faf3f33fd7c0a84a19087
Menai Lamers James <menai.james@warwick.ac.uk>
2026-03-13 04:23:33 -0400

    Add default empty filename

--- Fitters/FitterBase.h
+++ Fitters/FitterBase.h
@@ -47,7 +47,7 @@ class FitterBase {
   void RunLLHScan();
 
   /// @brief LLH scan is good first estimate of step scale
-  void GetStepScaleBasedOnLLHScan(const std::string& filename);
+  void GetStepScaleBasedOnLLHScan(const std::string& filename = "");
 
   /// @brief Perform a 2D likelihood scan.
   /// @warning This operation may take a significant amount of time, especially for complex models.
