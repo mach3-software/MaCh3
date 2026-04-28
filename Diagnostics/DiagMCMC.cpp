46d7a8759900fa269965af1035496e8b36461e9e
Alexander Richards <a.richards@imperial.ac.uk>
2026-04-28 18:52:53 +0000

    fixup

--- Diagnostics/DiagMCMC.cpp
+++ Diagnostics/DiagMCMC.cpp
@@ -3,13 +3,13 @@
 
 int main(int argc, char const* argv[]){
     MACH3LOG_WARN("Deprecation Warning: Use of the standalone executable will be deprecated in future releases.");
-    MACH3LOG_WARN("                   : you can use 'mach3 process' as a direct replacement instead.");
+    MACH3LOG_WARN("                   : you can use 'mach3 diag' as a direct replacement instead.");
     argv[0] = "diag";
     mach3::DiagMCMCPlugin proc;
     ArgumentParser* parser = proc.get_parser();
     parser->parse_args(argc, argv);
     proc.run();
     MACH3LOG_WARN("Deprecation Warning: Use of the standalone executable will be deprecated in future releases.");
-    MACH3LOG_WARN("                   : you can use 'mach3 process' as a direct replacement instead.");
+    MACH3LOG_WARN("                   : you can use 'mach3 diag' as a direct replacement instead.");
     return 0;
 }
