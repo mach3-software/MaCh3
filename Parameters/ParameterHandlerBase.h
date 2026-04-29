393f6016baf6aacf913b1cae219b898d9c9e3853
Kamil Skwarczynski <skwarczynskikamil@gmail.com>
2026-04-29 18:27:32 +0000

    Add more const correctness and furhter remove duplicates

--- Parameters/ParameterHandlerBase.h
+++ Parameters/ParameterHandlerBase.h
@@ -182,7 +182,7 @@ class ParameterHandlerBase {
   /// @details This function converts the covariance matrix to a correlation matrix and
   ///          returns a TH2D object, which can be used for advanced plotting purposes.
   /// @return A pointer to a TH2D object representing the correlation matrix
-  TH2D* GetCorrelationMatrix();
+  TH2D* GetCorrelationMatrix() const;
 
   /// @brief DB Pointer return to param position
   ///
@@ -193,7 +193,7 @@ class ParameterHandlerBase {
   /// push_back then the pointer is no longer valid... maybe need a better
   /// way to deal with this? It was fine before when the return was to an
   /// element of a new array. There must be a clever C++ way to be careful
-  const M3::float_t* RetPointer(const int iParam) {return &(_fPropVal.data()[iParam]);}
+  const M3::float_t* RetPointer(const int iParam) const {return &(_fPropVal.data()[iParam]);}
 
   /// @brief Get a reference to the proposed parameter values
   /// Can be useful if you want to track these without having to copy values using getProposed()
@@ -344,7 +344,7 @@ class ParameterHandlerBase {
                                 const std::vector<std::string>& FancyNames = {});
 protected:
   /// @brief Initialisation of the class using matrix from root file
-  void Init(const std::string& name, const std::string& file);
+  void InitFromFile(const std::string& name, const std::string& file);
   /// @brief Initialise vectors with parameters information
   /// @param size integer telling size to which we will resize all vectors/allocate memory
   void ReserveMemory(const int size);
