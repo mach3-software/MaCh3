3884242a7a0293bbee346b7cb2afd53aedc2863d
Alexander Richards <a.richards@imperial.ac.uk>
2026-05-01 11:00:08 +0000

    Implement the GetPenaltyTerm binary

---
+++ Diagnostics/GetPenaltyTermPlugin.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include "yaml-cpp/yaml.h"
+#include "api/plugin.hpp"
+
+namespace mach3{
+
+  class GetPenaltyTermPlugin: public IPlugin{
+
+    public:
+      virtual ~GetPenaltyTermPlugin(){
+        if (m_parser) { delete m_parser; } 
+      }
+      MaCh3ArgumentParser* get_parser() override;
+      int run() override;
+
+    private:
+      void ReadCovFile(const std::string& inputFile,
+                       std::vector <double>& Prior,
+                       std::vector <bool>& isFlat,
+                       std::vector<std::string>& ParamNames,
+                       std::vector<std::vector<double>>& invCovMatrix,
+                       int& nParams);
+      void LoadSettings(YAML::Node& Settings,
+                        std::vector<std::string>& SetsNames,
+                        std::vector<std::string>& FancyTitle,
+                        std::vector<std::vector<bool>>& isRelevantParam,
+                        const std::vector<std::string>& ParamNames,
+                        const int nParams);
+      void GetPenaltyTerm(const std::string& inputFile, const std::string& configFile);
+
+    private:
+      MaCh3ArgumentParser* m_parser;
+  };
+};
