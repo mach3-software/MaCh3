61df870fdc3e4d9959d90049e5cdf9aa41478808
Kamil Skwarczynski <skwarczynskikamil@gmail.com>
2026-05-13 14:18:29 +0000

    Preven using gaussian prior and circular prior

--- Parameters/ParameterHandlerBase.cpp
+++ Parameters/ParameterHandlerBase.cpp
@@ -148,6 +148,12 @@ void ParameterHandlerBase::EnableSpecialProposal(const YAML::Node& param, const 
                      _fLowBound.at(Index), _fUpBound.at(Index));
       throw MaCh3Exception(__FILE__, __LINE__);
     }
+    // KS: Make sure CircularPrior is applied only to param with flat prior. Sadly doesn't work with Gaussian
+    if(GetFlatPrior(Index) == false) {
+      MACH3LOG_ERROR("Enabled CircularPrior for parameter {}, which has gaussian prior", GetParFancyName(Index));
+      MACH3LOG_ERROR("This is not supported, CircularPrior only works with flat prior");
+      throw MaCh3Exception(__FILE__, __LINE__);
+    }
   }
 
   if (FlipEnabled) {
