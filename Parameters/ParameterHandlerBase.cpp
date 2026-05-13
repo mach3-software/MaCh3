5be897639224410821099b9ca0d278e7a209949b
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2026-05-13 16:31:52 +0100

    more helpful message after Henry comment

--- Parameters/ParameterHandlerBase.cpp
+++ Parameters/ParameterHandlerBase.cpp
@@ -152,6 +152,7 @@ void ParameterHandlerBase::EnableSpecialProposal(const YAML::Node& param, const 
     if(GetFlatPrior(Index) == false) {
       MACH3LOG_ERROR("Enabled CircularPrior for parameter {}, which has gaussian prior", GetParFancyName(Index));
       MACH3LOG_ERROR("This is not supported, CircularPrior only works with flat prior");
+      MACH3LOG_ERROR("Change FlatPrior in Parameter config to true");
       throw MaCh3Exception(__FILE__, __LINE__);
     }
   }
