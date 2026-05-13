aebe4aced27d808653569c55915bc151d55ebcc8
Alexander Richards <a.richards@imperial.ac.uk>
2026-05-13 11:53:04 +0000

    update

---
+++ cli/DynamicPlugin.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <dlfcn.h>
+#include "api/plugin.hpp"
+
+namespace mach3{
+    class DynamicPlugin: public IPlugin{
+        public:
+            DynamicPlugin(void* handle,
+                          IPlugin* plugin,
+                          destroy_plugin_t destroy_func):m_handle(handle),
+                                                         m_plugin(plugin),
+                                                         m_destroy_func(destroy_func){}
+
+            int run() {
+                return m_plugin->run();
+            }
+            MaCh3ArgumentParser* get_parser(){
+                return m_plugin->get_parser();
+            }
+
+            void destroy(){
+                m_destroy_func(m_plugin);
+                m_plugin = 0;
+                dlclose(m_handle);
+                m_handle = 0;
+                m_destroy_func = 0;
+            }
+
+        private:
+            void* m_handle;
+            IPlugin* m_plugin;
+            destroy_plugin_t m_destroy_func;
+    };
+};
