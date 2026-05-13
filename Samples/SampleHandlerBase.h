4907e13575360e5725fd09a56a35a5be85065409
Kamil Skwarczynski <skwarczynskikamil@gmail.com>
2026-05-13 12:30:06 +0000

    add depracation warning

--- Samples/SampleHandlerBase.h
+++ Samples/SampleHandlerBase.h
@@ -124,10 +124,10 @@ class SampleHandlerBase :  public SampleHandlerInterface
                                                     const std::string& ProjectionVar_StrY, const int kModeToFill = -1,
                                                     const int kChannelToFill = -1, const int WeightStyle = 0) final;
 
-  std::unique_ptr<TH1> GetModeHist1D(const int iSample, int s, int m, int style = 0) {
+  [[deprecated]] std::unique_ptr<TH1> GetModeHist1D(const int iSample, int s, int m, int style = 0) {
     return Get1DVarHistByModeAndChannel(iSample, GetKinVarName(iSample, 0), m, s, style);
   }
-  std::unique_ptr<TH2> GetModeHist2D(const int iSample, int s, int m, int style = 0) {
+  [[deprecated]] std::unique_ptr<TH2> GetModeHist2D(const int iSample, int s, int m, int style = 0) {
     return Get2DVarHistByModeAndChannel(iSample, GetKinVarName(iSample, 0), GetKinVarName(iSample, 1), m, s, style);
   }
 
