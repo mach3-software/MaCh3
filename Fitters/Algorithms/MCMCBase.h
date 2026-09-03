c20f6c34a82b0060943801aae30a55d2849dd6fa
Kamil Skwarczynski <skwarczynskikamil@gmail.com>
2026-09-03 12:37:47 +0000

    fix merge conflcits

--- Fitters/Algorithms/MCMCBase.h
+++ Fitters/Algorithms/MCMCBase.h
@@ -23,10 +23,6 @@ class MCMCBase : public FitterBase {
 
     /// @copydoc FitterBase::GetFancyName
     virtual std::string GetFancyName() const final;
-
-    /// @brief Set how long chain should be
-    /// @param L new chain length
-   void setChainLength(unsigned int L) { chainLength = L; };
  protected:
     /// @brief The full StartStep->DoStep->EndStep chain
     void DoMCMCStep();
