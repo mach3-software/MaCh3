79de86b90ae8c912f021927e32072334b8fa227b
Kamil Skwarczynski <skwarczynskikamil@gmail.com>
2026-09-14 16:34:17 +0000

    add fancy animation to visualise MCMC

--- Doc/setup_wiki.sh
+++ Doc/setup_wiki.sh
@@ -3,3 +3,4 @@
 #dnf install -y perl
 #dnf install -y doxygen
 git clone https://github.com/mach3-software/MaCh3.wiki.git Doc/User-Guide
+curl -L "https://github.com/KSkwarczynski/Python/blob/main/MaCh3/animation/MCMC_Example.mp4" -o Doc/MaCh3Web/MCMC_Example.mp4
