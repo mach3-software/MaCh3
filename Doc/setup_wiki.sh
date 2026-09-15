8e52bb7d46f69ee4a6048caa780e8b6eefec7e57
Kamil Skwarczynski <skwarczynskikamil@gmail.com>
2026-09-15 09:29:19 +0000

    fix setup_wiki

--- Doc/setup_wiki.sh
+++ Doc/setup_wiki.sh
@@ -3,4 +3,6 @@
 #dnf install -y perl
 #dnf install -y doxygen
 git clone https://github.com/mach3-software/MaCh3.wiki.git Doc/User-Guide
-curl -L "https://github.com/KSkwarczynski/Python/blob/main/MaCh3/animation/MCMC_Example.mp4" -o Doc/MaCh3Web/MCMC_Example.mp4
+curl -L "https://raw.githubusercontent.com/KSkwarczynski/Python/main/MaCh3/animation/MCMC_Example.mp4" -o Doc/MaCh3Web/MCMC_Example.mp4
+
+curl -L "https://raw.githubusercontent.com/KSkwarczynski/Python/main/MaCh3/animation/posterior_predictive_Example.mp4" -o Doc/MaCh3Web/posterior_predictive_Example.mp4
